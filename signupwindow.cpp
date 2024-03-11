ee79103cddc2ed5b43608f089e17f1cac8ddd688
knppkp <140078295+knppkp@users.noreply.github.com>
2024-03-11 14:23:04 +0700

    Add files via upload

--- signupwindow.cpp
+++ signupwindow.cpp
@@ -4,6 +4,7 @@
 #include "email.h"
 #include <regex>
 #include <QMessageBox>
+#include <QKeyEvent>
 #include <string>
 
 using namespace std;
@@ -13,6 +14,11 @@ SignupWindow::SignupWindow(QWidget *parent)
     , ui(new Ui::SignupWindow)
 {
     ui->setupUi(this);
+
+    ui->emailLineEdit->installEventFilter(this);
+    ui->usernameLineEdit->installEventFilter(this);
+    ui->passwordLineEdit->installEventFilter(this);
+
 }
 
 SignupWindow::~SignupWindow()
@@ -61,5 +67,45 @@ void SignupWindow::on_signupButton_clicked()
         verify = new SignupVerify(userEmail_String, username_String, userPassword_String, sentCode, member, this);
         verify->show();
     }
+}
+
+void SignupWindow::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_Return) {
+        event->accept();
+        on_signupButton_clicked();
+    } else {
+        QMainWindow::keyPressEvent(event);
+    }
+}
+
+bool SignupWindow::eventFilter(QObject *obj, QEvent *event)
+{
+    if (obj == ui->usernameLineEdit || obj == ui->passwordLineEdit || obj == ui->emailLineEdit) {
+        if (event->type() == QEvent::KeyPress) {
+            QKeyEvent *keyEvent = static_cast<QKeyEvent*>(event);
+            if (keyEvent->key() == Qt::Key_Up) {
+                // Handle Up arrow key press
+                QWidget *currentLineEdit = qobject_cast<QWidget *>(obj);
+                if (currentLineEdit == ui->usernameLineEdit) {
+                    ui->emailLineEdit->setFocus();
+                } else if (currentLineEdit == ui->passwordLineEdit) {
+                    ui->usernameLineEdit->setFocus();
+                } else if (obj == ui->emailLineEdit) {
+                    ui->passwordLineEdit->setFocus();
+                }
+            } else if (keyEvent->key() == Qt::Key_Down) {
+                // Handle Down arrow key press
+                QWidget *currentLineEdit = qobject_cast<QWidget *>(obj);
+                if (currentLineEdit == ui->usernameLineEdit) {
+                    ui->passwordLineEdit->setFocus();
+                } else if (currentLineEdit == ui->passwordLineEdit) {
+                    ui->emailLineEdit->setFocus();
+                } else if (obj == ui->emailLineEdit) {
+                    ui->usernameLineEdit->setFocus();
+                }
+            }
+        }
+    }
+    return QMainWindow::eventFilter(obj, event);
 }
 
